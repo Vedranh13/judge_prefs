d8385e8817c04fb97b7dd995c0e081cf62f5c949
Vedran Hadziosmanovic <vedranh13@gmail.com>
2016-09-21 02:17:17 -0700

    Created staticmethod to verify a judge data dictionary

--- server/global_vars.py
+++ server/global_vars.py
@@ -11,3 +11,6 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
+"""Explanation of debate terms:
+T : Short for topicality win rate ie what is the win rate when running a topicallity type argument with this judge
+K_af : Same as T but afirmative kritique argument"""
--- server/judge.py
+++ server/judge.py
@@ -11,11 +11,14 @@ class judge(object):
         self.first_name = self.data['first_name']
         self.last_name = self.data['last_name']
         self.spreading = float(self.data['spreading'])
+        self.T = float(self.data['T'])
         # TODO more metrics
     @classmethod
     def create_new_judge(cls, data):
         """Create a new judge in firebase from DATA dictionary, returns a new judge object"""
-        return cls(db.child("judges").push(data)['name'])
+        if cls.is_data_valid(data):
+            return cls(db.child("judges").push(data)['name'])
+        return None # TODO something intelligent here. Raise Exception maybe?
     @classmethod
     def init_judge_with_name(cls, first, last):
         # TODO Handel reps
@@ -24,3 +27,36 @@ class judge(object):
             if judge['last_name'] == last:
                 return cls(judge_id)
         return None
+    @staticmethod
+    def is_data_valid(data_dictionary):
+        """A function which checks that DATA_DICTIONARY has all the keys a judge object should have
+
+        >>> first = "john"
+        >>> last = "denero"
+        >>> data = { 'first' : first, 'last' : last }
+        >>> judge.is_data_valid(data)
+        False
+        >>> spread = 1.0
+        >>> T = 5.6
+        >>> data = { 'first' : first, 'last' : last, 'spreading' : spread, 'T' : 5.6 }
+        >>> judge.is_data_valid(data)
+        False
+        >>> data = { 'first_name' : first, 'last_name' : last, 'spreading' : spread, 'T' : 5.6 }
+        >>> judge.is_data_valid(data)
+        True
+        >>> data = { 'first_name' : "first", 'last_name' : "last", 'spreading' : 5.0, 'T' : 2}
+        >>> judge.is_data_valid(data)
+        False
+        >>> data = { 'first_name' : "first", 'last_name' : "last", 'spreading' : 5.0, 'T' : 2.0}
+        >>> judge.is_data_valid(data)
+        True
+        """
+        if 'first_name' not in data_dictionary or type(data_dictionary['first_name']) != str:
+            return False
+        if 'last_name' not in data_dictionary or type(data_dictionary['last_name']) != str:
+            return False
+        if 'spreading' not in data_dictionary or type(data_dictionary['spreading']) != float:
+            return False
+        if 'T' not in data_dictionary or type(data_dictionary['T']) != float: #Why does this not work? 'T' : 2
+            return False
+        return True
