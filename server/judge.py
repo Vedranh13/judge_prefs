a095802ec076eb686add21a170d936b00d863a5b
Vedran Hadziosmanovic <vedranh13@gmail.com>
2016-09-21 00:39:09 -0700

    Created the judge class which creates objects based on data from firebase and creates new entries in firebase

--- server/judge.py
+++ server/judge.py
@@ -1,4 +1,5 @@
 """Class repersenting a debate judge"""
+# TODO intelligent error checking
 from global_vars import db
 class judge(object):
     def __init__(self, judge_id):
@@ -6,7 +7,7 @@ class judge(object):
         # judge_id is a GUID for each judge
         self.judge_id = judge_id
         # query database at this point
-        self.data = db.child("judges").child(str(judge_id)).get().val()
+        self.data = db.child("judges").child(judge_id).get().val()
         self.first_name = self.data['first_name']
         self.last_name = self.data['last_name']
         self.spreading = float(self.data['spreading'])
@@ -16,6 +17,10 @@ class judge(object):
         """Create a new judge in firebase from DATA dictionary, returns a new judge object"""
         return cls(db.child("judges").push(data)['name'])
     @classmethod
-    def get_judge_id_name(cls, first, last):
-        """This method returns a new judge object given a FIRST and LAST name"""
-        pass
+    def init_judge_with_name(cls, first, last):
+        # TODO Handel reps
+        """This method returns a new judge object given a FIRST and LAST name. If no judge is found, return None"""
+        for judge_id, judge in db.child("judges").order_by_child('first_name').equal_to(first).get().val().items():
+            if judge['last_name'] == last:
+                return cls(judge_id)
+        return None
