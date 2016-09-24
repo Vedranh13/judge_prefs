461ac11e7f009f1390d6431939a02d7c8b450fb5
Evan Lohn <evan@airbears2-10-142-36-166.airbears2.1918.berkeley.edu>
2016-09-24 14:51:10 -0700

    commented out some potentially uneecessary code that was causing an error

--- judge_prefs/www/js/app.js
+++ judge_prefs/www/js/app.js
@@ -6,7 +6,6 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'firebase'])
-
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -16,14 +15,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
-    starter.initializeApp({
-        serviceAccount: {
-            projectId: "judge-prefs",
-            clientEmail: "fb-data@judge-prefs.iam.gserviceaccount.com",
-  PrivateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC1yMqDaqKVVxIg\nw5bX4uBUok3aNvvFDGn8nzf5R3I6BSHAPIhGYu8EsIhjvW63++V/jQctJjND80u3\n2T/RLJA4e7DqRZ+AfaV/vMjl36S++QgdraRgDxCDNcYknY7OQx5ov3UoFRa+AMM9\nnj3sZlpLVQNcfLdGexlulb0ix7b+wOfc0QXBirkesv5dnznIE6QsJl3KX/i38a+U\nKC0n65aId366DhfP7g4oGodEQDjbZ/5Fh1RwyX8LY07RhLK1+yDoyzOyZjD+tjFj\nZKFXbvWaJanJz7JQ3wQoNPixYdPu/fyOrmoubtqBGarafeAAmL/LD/3L49alA7iT\nkIJCQA6zAgMBAAECggEAHFvmu/m2w3WB7nqU3bkXJhzr6E8LmAIHxtFP+iObPlHu\nof7Pg6uLfj/MB0NWMn2pn+xwYirDdKZP+a56tjctiKJDR0j1SSZQ20yoSrRJD0Hq\ncjbUnee7u7x33N53mNX/uHel30rq8VlNHB/DqU2OaJIqpBMQxH4PyaCykOp3MebC\n2UzQpGX9XKw+/X887dyfnsofh7h23r2ZfXbWEWGJLejw6eqaSlmzXj9Znjz+7dcQ\nWjGKXLwf3tU0ouB441M17q6jDQwAqpDiUG3A8c2BekXdYVrDns7eyb3ad8fm94Ae\niWA33hjUmFeb1c5eYNPsRQ6WZiCYD2gZMgVB0SYWAQKBgQDkm/RG84GHKVJJYXli\nkSq/DSzNk48FXUyC5dDaBm9Vp87XwtBpQE4VrXEDIWZy/ndFfCbCpKRQb/vjmtTj\n6WmLWCf/jsFfC7WN9L36SnrGSgXV26xNL2LVV31cdf0wyL22JBkQiuBmI4HnIp5a\nqI9CAcviwnQ/c/S+ika3ZMqOwwKBgQDLkJhj1RTE6+w7wH+4cFxWnO2WLWbR6/Xl\nF7KUOiI/sIfUkMr/XQvLz4bdiT7NNCBxCwhRVwACQ4ovt/YF/YrxQs052yd7P+JE\n2gRdWLYBcmTxy+GRXFCTKCNF9NLXPumWtFRq6HJX5ExXnS0jNe0enOziB6iGXYYl\n2g2aOpthUQKBgQDcLLOs7fOPSKXe1MlCvEtuK/M9n2M8qGn7G/n1MgNifLcDKLbu\n16ccy6mveSihkiEvQ+5UwpsaeXAg+ssVIoWrVeFYCZaFwtI+ATxDZh4vAbXzDZo5\n0Gygyp2LQnvy0zOby9J0Ez0iQgpnoPjgmb03tmE2aU/qmPRb2vNWx/UqXwKBgQDG\nhtdIDah6wIecYoltLA+x3MwM+WxccF5YtbrQur1qFdTypt+DGpCFjXg/GDmqURsO\nhC7xfQKiMpJTJXsHrpTR5E9YDHa9ybj3YxR8oSan1JPECQ2NAVYHnk6ATFtP1qhi\n9K2bseTJ+PnHPJ+nxlDh2TNg4Q0nNZC926IGdv/QcQKBgHLEQjETyhKe07DIEyvN\nY+A8Ua+M7t0Y3+kunirioznaghfZSppQXwUp4hmLXug5fLIPsroQaG5SJyRx5cku\n5IbpE0789kiLYkvDWMp6x8Qbht3oBlNg1Kr8/RJ0aTNpuQklddtGWdFhheokRwg7\nCdMqsbQDA0wbkrT8x6AHXba+\n-----END PRIVATE KEY-----\n"
-            },
-            databaseURL:"https://judge-prefs.firebaseio.com"
-        });
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
@@ -113,4 +104,13 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   var ref = new Firebase("https://judge-prefs.firebaseio.com");
   $scope.authObj = $firebaseAuth(ref);
  }
-]);
+])
+//.initializeApp({
+//    serviceAccount: {
+//        projectId: "judge-prefs",
+//        clientEmail: "fb-data@judge-prefs.iam.gserviceaccount.com",
+//        PrivateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC1yMqDaqKVVxIg\nw5bX4uBUok3aNvvFDGn8nzf5R3I6BSHAPIhGYu8EsIhjvW63++V/jQctJjND80u3\n2T/RLJA4e7DqRZ+AfaV/vMjl36S++QgdraRgDxCDNcYknY7OQx5ov3UoFRa+AMM9\nnj3sZlpLVQNcfLdGexlulb0ix7b+wOfc0QXBirkesv5dnznIE6QsJl3KX/i38a+U\nKC0n65aId366DhfP7g4oGodEQDjbZ/5Fh1RwyX8LY07RhLK1+yDoyzOyZjD+tjFj\nZKFXbvWaJanJz7JQ3wQoNPixYdPu/fyOrmoubtqBGarafeAAmL/LD/3L49alA7iT\nkIJCQA6zAgMBAAECggEAHFvmu/m2w3WB7nqU3bkXJhzr6E8LmAIHxtFP+iObPlHu\nof7Pg6uLfj/MB0NWMn2pn+xwYirDdKZP+a56tjctiKJDR0j1SSZQ20yoSrRJD0Hq\ncjbUnee7u7x33N53mNX/uHel30rq8VlNHB/DqU2OaJIqpBMQxH4PyaCykOp3MebC\n2UzQpGX9XKw+/X887dyfnsofh7h23r2ZfXbWEWGJLejw6eqaSlmzXj9Znjz+7dcQ\nWjGKXLwf3tU0ouB441M17q6jDQwAqpDiUG3A8c2BekXdYVrDns7eyb3ad8fm94Ae\niWA33hjUmFeb1c5eYNPsRQ6WZiCYD2gZMgVB0SYWAQKBgQDkm/RG84GHKVJJYXli\nkSq/DSzNk48FXUyC5dDaBm9Vp87XwtBpQE4VrXEDIWZy/ndFfCbCpKRQb/vjmtTj\n6WmLWCf/jsFfC7WN9L36SnrGSgXV26xNL2LVV31cdf0wyL22JBkQiuBmI4HnIp5a\nqI9CAcviwnQ/c/S+ika3ZMqOwwKBgQDLkJhj1RTE6+w7wH+4cFxWnO2WLWbR6/Xl\nF7KUOiI/sIfUkMr/XQvLz4bdiT7NNCBxCwhRVwACQ4ovt/YF/YrxQs052yd7P+JE\n2gRdWLYBcmTxy+GRXFCTKCNF9NLXPumWtFRq6HJX5ExXnS0jNe0enOziB6iGXYYl\n2g2aOpthUQKBgQDcLLOs7fOPSKXe1MlCvEtuK/M9n2M8qGn7G/n1MgNifLcDKLbu\n16ccy6mveSihkiEvQ+5UwpsaeXAg+ssVIoWrVeFYCZaFwtI+ATxDZh4vAbXzDZo5\n0Gygyp2LQnvy0zOby9J0Ez0iQgpnoPjgmb03tmE2aU/qmPRb2vNWx/UqXwKBgQDG\nhtdIDah6wIecYoltLA+x3MwM+WxccF5YtbrQur1qFdTypt+DGpCFjXg/GDmqURsO\nhC7xfQKiMpJTJXsHrpTR5E9YDHa9ybj3YxR8oSan1JPECQ2NAVYHnk6ATFtP1qhi\n9K2bseTJ+PnHPJ+nxlDh2TNg4Q0nNZC926IGdv/QcQKBgHLEQjETyhKe07DIEyvN\nY+A8Ua+M7t0Y3+kunirioznaghfZSppQXwUp4hmLXug5fLIPsroQaG5SJyRx5cku\n5IbpE0789kiLYkvDWMp6x8Qbht3oBlNg1Kr8/RJ0aTNpuQklddtGWdFhheokRwg7\nCdMqsbQDA0wbkrT8x6AHXba+\n-----END PRIVATE KEY-----\n"
+//        },
+//        databaseURL:"https://judge-prefs.firebaseio.com"
+//    });
+;
