92431fe7bda57166020600f4702a02428019a763
Vedran Hadziosmanovic <vedranh13@gmail.com>
2016-09-24 14:57:10 -0700

    Can get paradigms from both tb and judgephiliosophies

--- server/sites.py
+++ server/sites.py
@@ -12,7 +12,16 @@ class site(object):
         self.soup = BeautifulSoup(self.html, 'html.parser')
     def update_judge_phil(self, first_name, last_name):
         jud = judge.init_judge_with_name(first_name, last_name)
+        self.query_for_judge(first_name, last_name)
         jud.update_field('phil', self.get_philiosophy())
+    def get_philiosophy_base(self, class_string):
+        """This function extracts the judge paradigm from the raw html"""
+        #TODO abstract later
+        for div in self.soup.find_all('div'):
+            if div.get('class'):
+                if class_string in div.get('class'):
+                    print(div.get_text())
+                    return div.get_text()
 class tabroom(site):
     def __init__(self):
         super().__init__("www.tabroom.com/index/paradigm.mhtml")
@@ -21,20 +30,13 @@ class tabroom(site):
         res = requests.get(self.url + "?search_first=" + first_name + "&search_last=" + last_name)
         self.update_html(res.text)
     def get_philiosophy(self):
-        """This function extracts the judge paradigm from the raw html"""
-        #TODO abstract later
-        for div in self.soup.find_all('div'):
-            if div.get('class'):
-                if 'paradigm' in div.get('class'):
-                    print("got phil")
-                    return div.get_text()
+        return self.get_philiosophy_base('paradigm')
 class judge_phil(site):
     def __init__(self):
         super().__init__("judgephilosophies.wikispaces.com/")
     def query_for_judge(self, first_name, last_name):
         #TODO abstract later
-        print(self.url + last_name + "%2C+" + first_name)
         res = requests.get(self.url + last_name + "%2C+" + first_name)
         self.update_html(res.text)
     def get_philiosophy(self):
-        pass
+        return self.get_philiosophy_base('commentContainer')
