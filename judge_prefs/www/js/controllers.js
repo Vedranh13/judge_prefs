331a110356be68d1d19dd6cd2d0bdbac63bbb2ce
Evan Lohn <evan@airbears2-10-142-36-166.airbears2.1918.berkeley.edu>
2016-09-24 17:24:03 -0700

    got rid of a bad merge mark

--- judge_prefs/www/js/controllers.js
+++ judge_prefs/www/js/controllers.js
@@ -2,7 +2,6 @@ angular.module('starter.controllers', ['firebase','ionic'])
 
 .controller('DashCtrl', function($scope) {})
 
-<<<<<<< HEAD
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
