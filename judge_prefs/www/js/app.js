be05cc0c7ea9f871afe4ff33bf2bd1db162fd694
qwertyuiop <qwertyuiop@airbears2-10-142-39-237.airbears2.1918.berkeley.edu>
2016-09-24 17:03:00 -0700

    got rid of default tabs

--- judge_prefs/www/js/app.js
+++ judge_prefs/www/js/app.js
@@ -88,47 +88,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
         controller: 'rrCtrl'
       }
     }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-    .state('tab.search', {
-      url: '/search',
-      views: {
-        'tab-search': {
-          templateUrl: 'templates/tab-search.html',
-          controller: 'searchCtrl'
-        }
-      }
-    })
-
-
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
   });
 
   // if none of the above states are matched, use this as the fallback
