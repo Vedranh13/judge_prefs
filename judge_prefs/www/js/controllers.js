fa1ecc18882c0a3b01d775f772c903623ed05921
qwertyuiop <qwertyuiop@airbears2-10-142-101-144.airbears2.1918.berkeley.edu>
2016-09-26 02:08:32 -0700

    firebase integration is almost done

--- judge_prefs/www/js/controllers.js
+++ judge_prefs/www/js/controllers.js
@@ -35,31 +35,66 @@ angular.module('starter.controllers', ['firebase','ionic'])
   };
 })
 
-.controller('searchCtrl', function($scope, $state, $firebaseArray, $ionicPopup, $rootScope)  {
- // var ref = new Firebase("http://judge-prefs.firebaseio.com/judges");
- // var judges = $firebaseArray(ref);
- $scope.finder = {};
- $scope.searchNext = function() {
-   var judgeFound = 0;
-   var ref = new Firebase("https://judge-prefs.firebaseio.com/");
-   var array = $firebaseArray(ref.child("judges"));
-   array.$loaded().then(function(array) {
-     alert("database loaded");
-    //  for(var i = 0; i < judges.length; i++) {
-    // if (($scope.finder.f.toLowerCase() == judges[i].first_name) && ($scope.finder.l.toLowerCase() == judges[i].last_name)) {
-    //    judgeFound=1;
-    //    $rootScope.jid = judges[i].$id;
-    //    alert($rootScope.jid);
-    //    $state.go('sj-results');
-  //    }
-  //  }
-  //   if(judgeFound===0){
-  //     var alertPopupThree = $ionicPopup.alert({
-  //       title: "Judge Not Found."
-  //       });
-  //  }
-});
-};})
+.controller('searchCtrl', function($scope, $state, $firebaseObject, $ionicPopup, $rootScope) {
+
+  $scope.finder = {};
+
+  var ref = new Firebase("https://judge-prefs.firebaseio.com/judges");
+
+  $scope.searchNext = function() {
+
+    var firstNameLower = $scope.finder.f.toLowerCase();
+    var lastNameLower = $scope.finder.l.toLowerCase();
+
+    ref.orderByChild("last_name").equalTo(lastNameLower).on("child_added", function(snapshot) {
+      if (snapshot.child("first_name").val() == firstNameLower) {
+
+      $rootScope.first_name = firstNameLower;
+      $rootScope.last_name = lastNameLower;
+      $rootScope.spreading = snapshot.child("spreading").val();
+
+      $rootScope.k_aff_wr = snapshot.child("k_aff_wr").val();
+      $rootScope.trad_aff_wr = snapshot.child("trad_aff_wr").val();
+
+      $rootScope.t_aff_wr = snapshot.child("T").child("aff_wr").val();
+      $rootScope.t_we_meet_p = judges[i].T.we_meet_p;
+      $rootScope.t_aff_flex_outweighs = judges[i].T.aff_flex_outweighs;
+      $rootScope.t_reasonability_p = judges[i].T.reasonability_p;
+      $rootScope.t_condo_p = judges[i].T.condo_p;
+
+      $rootScope.k_aff_wr = judges[i].K.aff_wr;
+      $rootScope.k_framework_wr = judges[i].K.framework_wr;
+      $rootScope.k_case_outweighs_wr = judges[i].K.case_outweighs_wr;
+      $rootScope.k_perm_wr = judges[i].K.perm_wr;
+      $rootScope.k_impact_turn_wr = judges[i].K.impact_turn_wr;
+      $rootScope.k_no_alt_solvency_wr = judges[i].K.no_alt_solvency_wr;
+      $rootScope.k_condo_wr = judges[i].K.condo_wr;
+
+      $rootScope.cp_aff_wr = judges[i].CP.aff_wr;
+      $rootScope.cp_condo_wr = judges[i].CP.condo_wr;
+      $rootScope.cp_perm_wr = judges[i].CP.perm_wr;
+      $rootScope.cp_cp_theory_wr = judges[i].CP.cp_theory_wr;
+      $rootScope.cp_offense_on_net_benefit = judges[i].CP.offense_on_net_benefit;
+      $rootScope.cp_links_to_net_benefit = judges[i].CP.links_to_net_benefit;
+      $rootScope.cp_solvency_deficit = judges[i].CP.solvency_deficit;
+
+      $rootScope.da_aff_wr = judges[i].DA.aff_wr;
+      $rootScope.da_condo_wr = judges[i].DA.condo_wr;
+      $rootScope.da_case_outweighs_wr = judges[i].DA.case_outweighs_wr;
+      $rootScope.da_no_link_wr = judges[i].DA.no_link_wr;
+      $rootScope.da_link_turn_wr = judges[i].DA.link_turn_wr;
+      $rootScope.da_no_impact_wr = judges[i].DA.no_impact_wr;
+      $rootScope.da_impact_turn_wr = judges[i].DA.impact_turn_wr;
+
+      $rootScope.impact_turn_aff_wr = judges[i].impact_turn.aff_wr;
+
+      $rootScope.phil = judges[i].phil;
+      }
+    });
+
+  };
+
+})
 
 
 .controller('rr-tCtrl', function($scope, $rootScope, $state, $firebaseArray, $ionicPopup) {
